c3e8c367e1a5586b5dbeabef3930a046b0a1be55
Thomas-Lim <sunwooim89@gmail.com>
2020-11-14 16:35:13 +0900

    Seventh Step - MariaDB on AWS

--- src/main/java/com/aws/deploy/CRUDAWStest/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/aws/deploy/CRUDAWStest/config/auth/dto/OAuthAttributes.java
@@ -9,7 +9,6 @@ import java.util.Map;
 
 @Getter
 public class OAuthAttributes {
-
     private Map<String, Object> attributes;
     private String nameAttributeKey;
     private String name;
@@ -17,12 +16,7 @@ public class OAuthAttributes {
     private String picture;
 
     @Builder
-    public OAuthAttributes(Map<String, Object> attributes,
-                           String nameAttributeKey,
-                           String name,
-                           String email,
-                           String picture) {
-
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
         this.attributes = attributes;
         this.nameAttributeKey = nameAttributeKey;
         this.name = name;
@@ -30,11 +24,10 @@ public class OAuthAttributes {
         this.picture = picture;
     }
 
-    public static OAuthAttributes of(String registrationId,
-                                 String userNameAttributeName,
-                                 Map<String, Object> attributes) {
-        // naver 추가
-        if("naver".equals(registrationId)) {
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+
+        // 네이버 추가가
+       if("naver".equals(registrationId)) {
             return ofNaver("id", attributes);
         }
         return ofGoogle(userNameAttributeName, attributes);
@@ -53,18 +46,18 @@ public class OAuthAttributes {
     }
     
     // Naver 로그인 영역
-    private static OAuthAttributes ofNaver(String userNameAttributeName,
-                                            Map<String, Object> attributes) {
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
         Map<String, Object> response = (Map<String, Object>) attributes.get("response");
-        
+
         return OAuthAttributes.builder()
-                .name((String) attributes.get("name"))
-                .email((String) attributes.get("email"))
-                .picture((String) attributes.get("profile_image"))
-                .attributes(attributes)
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
+
 
     // User 엔티티 생성. 가입 시 엔티티 생성인데, 기본 권한은 GUEST.
     public User toEntity() {
@@ -72,7 +65,7 @@ public class OAuthAttributes {
                 .name(name)
                 .email(email)
                 .picture(picture)
-                .role((Role.GUEST))
+                .role(Role.GUEST)
                 .build();
     }
     // 해당 클래스 생성이 끝났으면 같은 패키지에 SessionUser 클래스를 생성
